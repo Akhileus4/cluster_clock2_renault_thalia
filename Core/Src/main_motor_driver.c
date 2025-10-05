75dd1603c2cd265baee15744d9636b6b76697048
Akhileus4 <40799969+Akhileus4@users.noreply.github.com>
2025-10-05 19:18:58 +0200

    motor step to kmh, tested it in main

--- Core/Src/main_motor_driver.c
+++ Core/Src/main_motor_driver.c
@@ -81,3 +81,65 @@ void motor_move_to(int target_pos) {
 	motor_rotate(direction, steps); // hívjuk a meglévő függvényt
 	motor_position = target_pos;    // frissítjük az aktuális pozíciót
 }
+
+void motor_move_to_kmh(int kmh) {
+	// Korlátozzuk a kmh értéket
+	if (kmh < 10)
+		kmh = 10;
+	if (kmh > 210)
+		kmh = 210;
+
+	// Lookup table a leggyakoribb értékekhez
+	typedef struct {
+		int kmh;
+		int position;
+	} kmh_position_map_t;
+
+	static const kmh_position_map_t position_map[] = { { 10, 0 }, 	//updated
+			{ 20, 19 },		//updated
+			{ 30, 38 }, 	//updated
+			{ 40, 51 }, 	//updated
+			{ 50, 73 },		//updated
+			{ 60, 90 },		//updated
+			{ 70, 106 }, 	//updated
+			{ 80, 122 }, 	//updated
+			{ 90, 137 }, 	//updated
+			{ 100, 153 }, 	//updated
+			{ 110, 169 }, 	//updated
+			{ 120, 185 }, 	//updated
+			{ 130, 202 }, 	//updated
+			{ 140, 218 }, 	//updated
+			{ 150, 238 }, 	//updated
+			{ 160, 253 }, 	//updated
+			{ 170, 270 }, 	//updated
+			{ 180, 285 }, 	//updated
+			{ 190, 305 }, 	//updated
+			{ 200, 320 }, 	//updated
+			{ 210, 339 } }; //updated
+
+	int target_position = 0;
+	int map_size = sizeof(position_map) / sizeof(position_map[0]);
+
+	// Keressük meg a pontos egyezést vagy interpoláljunk
+	for (int i = 0; i < map_size; i++) {
+		if (position_map[i].kmh == kmh) {
+			target_position = position_map[i].position;
+			break;
+		}
+		// Ha a kmh érték a jelenlegi és következő pont között van
+		if (i < map_size - 1 && kmh > position_map[i].kmh
+				&& kmh < position_map[i + 1].kmh) {
+			// Lineáris interpoláció a két pont között
+			int kmh1 = position_map[i].kmh;
+			int pos1 = position_map[i].position;
+			int kmh2 = position_map[i + 1].kmh;
+			int pos2 = position_map[i + 1].position;
+
+			target_position = pos1
+					+ (kmh - kmh1) * (pos2 - pos1) / (kmh2 - kmh1);
+			break;
+		}
+	}
+
+	motor_move_to(target_position);
+}
